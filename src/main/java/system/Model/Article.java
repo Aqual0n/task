93d727f0e457b4878dadcf0be8cb60ae2222fbe4
Aqualon <lashkin200936@gmail.com>
2019-02-13 22:01:02 +0400

    redesigned project structure

---
+++ src/main/java/system/Model/Article.java
@@ -0,0 +1,52 @@
+package system.Model;
+
+import org.springframework.data.annotation.Id;
+
+public class Article {
+    @Id
+    private String id;
+
+    private String ArticleTitle;
+
+    private String ArticleText;
+
+    public Article(String articleTitle, String articleText) {
+        ArticleTitle = articleTitle;
+        ArticleText = articleText;
+    }
+
+    public Article() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getArticleTitle() {
+        return ArticleTitle;
+    }
+
+    public void setArticleTitle(String articleTitle) {
+        ArticleTitle = articleTitle;
+    }
+
+    public String getArticleText() {
+        return ArticleText;
+    }
+
+    public void setArticleText(String articleText) {
+        ArticleText = articleText;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "ArticleTitle='" + ArticleTitle + '\'' +
+                ", ArticleText='" + ArticleText + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/system/Service/CommentService.java
@@ -0,0 +1,78 @@
+package system.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import system.DAO.MongoFactory;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import system.Model.Comment;
+
+@Service("CommentaryService")
+@Transactional
+public class CommentService {
+
+    private static String dbName = "comments", dbCollection = "comments";
+    private static Logger log = Logger.getLogger(CommentService.class);
+
+    //Получение всех комментариев из БД
+    public List getAll() {
+        List commentList = new ArrayList();
+        DBCollection coll = MongoFactory.getCollection(dbName, dbCollection);
+
+        DBCursor cursor = coll.find();
+        while(cursor.hasNext()) {
+            DBObject dbObject = cursor.next();
+
+            Comment comment = new Comment();
+            comment.setId(dbObject.get("_id").toString());
+            comment.setAuthorName(dbObject.get("authorName").toString());
+            comment.setCommentaryText(dbObject.get("commentText").toString());
+            comment.setCommentaryDate(dbObject.get("commentDate").toString());
+            log.debug(comment);
+            // Adding the comment details to the list.
+            commentList.add(comment);
+        }
+        log.debug("Итого из БД получено= " + commentList.size() + " комментариев");
+        return commentList;
+    }
+
+    public Boolean add(Comment comment) {
+        boolean output = false;
+        Random ran = new Random();
+        log.debug("Adding a new commentary to the mongo database; Entered authorName is= " + comment.getAuthorName());
+        log.debug("Adding a new commentary to the mongo database; Entered commentaryText is= " + comment.getCommentaryText());
+        log.debug("Adding a new commentary to the mongo database; Entered commentaryDate is= " + comment.getCommentaryDate());
+
+        try {
+            DBCollection coll = MongoFactory.getCollection(dbName, dbCollection);
+            Date dateNow = new Date();
+            String dateString = dateNow.toString();
+            comment.setCommentaryDate(dateString);
+
+            // Создание нового объекта и присвоение деталей.
+            BasicDBObject doc = new BasicDBObject();
+            doc.put("_id", String.valueOf(ran.nextInt(100)));
+            doc.put("authorName", comment.getAuthorName());
+            doc.put("commentText", comment.getCommentaryText());
+            doc.put("commentDate", comment.getCommentaryDate());
+
+            // Save a new user to the mongo collection.
+            coll.insert(doc);
+            output = true;
+        } catch (Exception e) {
+            log.error("An error occurred while saving a new user to the mongo database", e);
+        }
+        return output;
+    }
+
+
+}
