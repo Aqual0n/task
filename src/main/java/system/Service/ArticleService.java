106a9050c889bbe5602c18c1d4a628f6ccb3a86c
Aqualon <lashkin200936@gmail.com>
2019-02-16 01:52:39 +0400

    truly working version //TODO: encoding and data formatting

--- src/main/java/system/Service/ArticleService.java
+++ src/main/java/system/Service/ArticleService.java
@@ -20,8 +20,8 @@ public class ArticleService implements ArticleServiceInterface{
     }
 
     @Override
-    public void add(Article article, InputStream image) {
-        articleDAO.add(article, image);
+    public void add(Article article) {
+        articleDAO.add(article);
     }
 
     @Override
